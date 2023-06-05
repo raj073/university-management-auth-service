4d0b3ca53a76153a64f649fa34205886aab295f6
raj073 <rajoni.das@outlook.com>
2023-06-05 19:17:11 +0600

    [Done]Setup express, mongoose, cors parser

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database Connected Sucessfully`);
+
+    app.listen(config.port, () => {
+      console.log(`University Management Server Listening on ${config.port}`);
+    });
+  } catch (error) {
+    console.log(`Failed to Connect Database, ${error}`);
+  }
+}
+
+main();
